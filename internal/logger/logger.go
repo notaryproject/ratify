f9e6b112fefe95ea9919f771725fbd3d1dd2d516
Xinhe Li <xinhl@microsoft.com>
2026-07-30 19:42:43 +1000

    chore: switch internal/logger to local v2 errors package (#2860)
    Signed-off-by: xinhl <xinhl@microsoft.com>

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -24,8 +24,8 @@ import (
 	logstash "github.com/bshuster-repo/logrus-logstash-hook"
 	dcontext "github.com/docker/distribution/context"
 	"github.com/google/uuid"
+	re "github.com/notaryproject/ratify/v2/errors"
 	icontext "github.com/notaryproject/ratify/v2/internal/context"
-	re "github.com/ratify-project/ratify/errors"
 	"github.com/sirupsen/logrus"
 )
 
